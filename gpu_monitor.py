93c83c328c4e3375100cbc2508a00991fa9ecb17
GanymedeNil <ganymedenil@gmail.com>
2024-06-13 22:30:29 +0800

    Support when the parent process is idle but the child process is working

--- gpu_monitor.py
+++ gpu_monitor.py
@@ -23,19 +23,25 @@ while True:
         if len(processes) > 0:
             processes = GpuProcess.take_snapshots(processes.values(), failsafe=True)
             processes.sort(key=lambda p: (p.username, p.pid))
+
+            gpu_process_tree = {}
+            pid_to_snapshot = {snapshot.pid: snapshot for snapshot in processes}
             for snapshot in processes:
-                key = (device.index, snapshot.pid)
-                current_active_processes.add(key)
-
-                # Update tracker for active processes
-                if snapshot.gpu_sm_utilization == 0 and snapshot.cpu_percent <= 1.0:
-                    inactive_tracker[key] = inactive_tracker.get(key,0) + 1
+                if snapshot.ppid in gpu_process_tree:
+                    gpu_process_tree[snapshot.ppid].append(snapshot.pid)
                 else:
-                    inactive_tracker[key] = 0
+                    gpu_process_tree[snapshot.pid] = [snapshot.pid]
 
-                # Kill process if it meets the criteria
+            for ppid, pids in gpu_process_tree.items():
+                key = (device.index, ppid)
+                current_active_processes.add(key)
+                for pid in pids:
+                    if pid_to_snapshot[pid].gpu_sm_utilization == 0 and pid_to_snapshot[pid].cpu_percent <= 1.0:
+                        inactive_tracker[key] = inactive_tracker.get(key, 0) + 1
+                    else:
+                        inactive_tracker[key] = 0
                 if inactive_tracker[key] >= THRESHOLD:
-                    kill_process(snapshot.pid)
+                    kill_process(ppid)
                     inactive_tracker.pop(key, None)
 
     # Clean up tracker for any process not active anymore
