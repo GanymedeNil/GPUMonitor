d4140747e4e2b78099524ffa048957c5a2bb7c67
GanymedeNil <ganymedenil@gmail.com>
2024-06-13 22:51:23 +0800

    Support when the parent process is idle but the child process is working

--- gpu_monitor.py
+++ gpu_monitor.py
@@ -1,17 +1,19 @@
 import time
 from nvitop import Device, GpuProcess
 import os
-
+import signal
 
 def kill_process(pid):
     try:
-        os.kill(pid, 9)
+        os.kill(pid, signal.SIGKILL)
     except Exception:
         pass
 
 
-INTERVAL = 5
-THRESHOLD = 12
+INTERVAL = 1
+THRESHOLD = 3
+GPU_SM_UTILIZATION_THRESHOLD = 0
+CPU_PERCENT_THRESHOLD = 1.0
 
 inactive_tracker = {}
 
@@ -21,6 +23,7 @@ while True:
     for device in devices:
         processes = device.processes()
         if len(processes) > 0:
+            time.sleep(1)
             processes = GpuProcess.take_snapshots(processes.values(), failsafe=True)
             processes.sort(key=lambda p: (p.username, p.pid))
 
@@ -37,7 +40,7 @@ while True:
                 current_active_processes.add(key)
                 tmp_status = 0
                 for pid in pids:
-                    if pid_to_snapshot[pid].gpu_sm_utilization == 0 and pid_to_snapshot[pid].cpu_percent <= 1.0:
+                    if pid_to_snapshot[pid].gpu_sm_utilization == GPU_SM_UTILIZATION_THRESHOLD and pid_to_snapshot[pid].cpu_percent <= CPU_PERCENT_THRESHOLD:
                         tmp_status += 1
                     else:
                         tmp_status = 0
@@ -46,7 +49,6 @@ while True:
                     inactive_tracker[key] = inactive_tracker.get(key, 0) + 1
                 else:
                     inactive_tracker[key] = 0
-
                 if inactive_tracker[key] >= THRESHOLD:
                     kill_process(ppid)
                     inactive_tracker.pop(key, None)
